26e38071754f3ff648d2878c85c7b3dc104e2548
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-01-04 17:01:06 +0100

    List messages in home page

--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
+import Message from '../models/Message';
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+  const messages = await Message.find().sort({ timestamp: -1 }).populate('user').exec();
+
+  res.render('index', {
+    messages,
+  });
+}));
 
 const indexRouter = router;
 export default indexRouter;
